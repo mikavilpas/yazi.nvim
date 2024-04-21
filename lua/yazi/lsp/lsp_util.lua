f3ccc14fd1c685dc16ad6db77154dc5a804093ec
Mika Vilpas <mika.vilpas@gmail.com>
2024-04-21 12:38:01 +0300

    fix(lsp): renaming only notified the lsp for the current file
    In projects where there are many types of files, such as readme files
    (markdown) and code files (let's say, typescript), there was an
    inconvenience that the currently active lsp servers would not be
    notified unless they were connected to the current buffer.
    
    That means that if you were editing a markdown file and wanted to rename
    a typescript file using yazi, the lsp server for typescript would not be
    notified of the rename.

--- lua/yazi/lsp/lsp_util.lua
+++ lua/yazi/lsp/lsp_util.lua
@@ -2,14 +2,13 @@ local M = {}
 
 -- For nvim 0.9 compatibility
 ---@param lsp_method string
----@param bufnr number
-function M.get_clients(lsp_method, bufnr)
+function M.get_clients(lsp_method)
   -- TODO remove this when we drop support for nvim 0.9
 
   ---@diagnostic disable-next-line: deprecated - but needed for compatibility
-  local clients = vim.lsp.get_active_clients({ bufnr = bufnr })
+  local clients = vim.lsp.get_active_clients()
   return vim.tbl_filter(function(client)
-    return client.supports_method(lsp_method, { bufnr = bufnr })
+    return client.supports_method(lsp_method)
   end, clients)
 end
 
