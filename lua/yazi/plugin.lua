7fe83722878110fff575f54856f7ed7479a4ddf4
Mika Vilpas <mika.vilpas@gmail.com>
2024-05-26 16:40:38 +0300

    feat(plugins): add some sanity checking and error reporting

--- lua/yazi/plugin.lua
+++ lua/yazi/plugin.lua
@@ -12,7 +12,7 @@ local M = {}
 ---   lazy = true,
 ---   build = function(plugin)
 ---     -- this will be called by lazy.nvim after the plugin was updated
----     require("yazi.plugin").build(plugin)
+---     require("yazi.plugin").build_plugin(plugin)
 ---   end,
 --- }
 --- ```
@@ -21,27 +21,56 @@ local M = {}
 ---
 ---@param plugin YaziLazyNvimPlugin
 ---@param options? { yazi_dir: string }
+---@return YaziPluginInstallationResultSuccess | YaziPluginInstallationResultFailure
 function M.build_plugin(plugin, options)
   local yazi_dir = options and options.yazi_dir
     or vim.fn.expand('~/.config/yazi')
   local to = vim.fs.normalize(vim.fs.joinpath(yazi_dir, 'plugins', plugin.name))
 
-  if not vim.fn.isdirectory(plugin.dir) then
-    vim.notify(
-      vim.inspect({ 'yazi.nvim: plugin directory does not exist', plugin.dir })
-    )
-    return
+  local dir = vim.loop.fs_stat(plugin.dir)
+  if dir == nil or dir.type ~= 'directory' then
+    ---@type YaziPluginInstallationResultFailure
+    local result = {
+      error = 'plugin directory does not exist',
+      from = plugin.dir,
+      message = 'yazi.nvim: failed to install plugin',
+    }
+    vim.notify(vim.inspect(result))
+    return result
   end
 
-  vim.notify(
-    vim.inspect({ 'yazi.nvim: installing plugin', from = plugin.dir, to = to })
-  )
-  vim.uv.fs_symlink(plugin.dir, to)
+  local success, error = vim.uv.fs_symlink(plugin.dir, to)
+
+  if not success then
+    ---@type YaziPluginInstallationResultFailure
+    local result = {
+      message = 'yazi.nvim: failed to install plugin',
+      from = plugin.dir,
+      to = to,
+      error = error or 'unknown error',
+    }
+    vim.notify(vim.inspect(result))
+
+    return result
+  end
+
+  ---@type YaziPluginInstallationResultSuccess
+  local result = {
+    message = 'yazi.nvim: successfully installed plugin ' .. plugin.name,
+    from = plugin.dir,
+    to = to,
+  }
+
+  vim.notify(vim.inspect(result))
+  return result
 end
 
 --- Represents information about the plugin to install. Note that this is
 --- compatible with LazyPlugin, the lazy.nvim plugin specification table, so
 --- you can just pass that.
 ---@alias YaziLazyNvimPlugin { name: string, dir: string }
+
+---@alias YaziPluginInstallationResultSuccess { message: string, from: string, to: string }
+---@alias YaziPluginInstallationResultFailure { message: string, from: string, to?: string, error: string }
 
 return M
