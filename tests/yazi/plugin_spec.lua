9948245f5f6b670e2853b65052e5695395accf45
Mika Vilpas <mika.vilpas@gmail.com>
2024-06-02 18:47:20 +0300

    feat(plugins): support including yazi flavors with `.build_flavor`

--- tests/yazi/plugin_spec.lua
+++ tests/yazi/plugin_spec.lua
@@ -15,41 +15,63 @@ describe('installing a plugin', function()
     vim.fn.delete(base_dir, 'rf')
   end)
 
-  it('can install if everything goes well', function()
-    local plugin_dir = vim.fs.joinpath(base_dir, 'test-plugin')
-    local yazi_dir = vim.fs.joinpath(base_dir, 'fake-yazi-dir')
+  describe('installing a plugin', function()
+    it('can install if everything goes well', function()
+      local plugin_dir = vim.fs.joinpath(base_dir, 'test-plugin')
+      local yazi_dir = vim.fs.joinpath(base_dir, 'fake-yazi-dir')
 
-    vim.fn.mkdir(plugin_dir)
-    vim.fn.mkdir(yazi_dir)
-    vim.fn.mkdir(vim.fs.joinpath(yazi_dir, 'plugins'))
+      vim.fn.mkdir(plugin_dir)
+      vim.fn.mkdir(yazi_dir)
+      vim.fn.mkdir(vim.fs.joinpath(yazi_dir, 'plugins'))
 
-    plugin.build_plugin({
-      dir = plugin_dir,
-      name = 'test-plugin',
-    }, { yazi_dir = yazi_dir })
+      plugin.build_plugin({
+        dir = plugin_dir,
+        name = 'test-plugin',
+      }, { yazi_dir = yazi_dir })
 
-    -- verify that the plugin was symlinked
-    -- yazi_dir/plugins/test-plugin -> plugin_dir
-    local symlink =
-      vim.loop.fs_readlink(vim.fs.joinpath(yazi_dir, 'plugins', 'test-plugin'))
+      -- verify that the plugin was symlinked
+      -- yazi_dir/plugins/test-plugin -> plugin_dir
+      local symlink =
+        vim.uv.fs_readlink(vim.fs.joinpath(yazi_dir, 'plugins', 'test-plugin'))
 
-    assert.are.same(plugin_dir, symlink)
+      assert.are.same(plugin_dir, symlink)
+    end)
+
+    it('warns the user if the plugin directory does not exist', function()
+      local plugin_dir = vim.fs.joinpath(base_dir, 'test-plugin')
+      local yazi_dir = vim.fs.joinpath(base_dir, 'fake-yazi-dir')
+      vim.fn.mkdir(yazi_dir)
+
+      local result = plugin.build_plugin({
+        dir = plugin_dir,
+        name = 'test-plugin-2',
+      }, { yazi_dir = yazi_dir })
+
+      assert.is_equal(
+        result.error,
+        'yazi plugin/flavor directory does not exist'
+      )
+      assert.is_equal(result.from, plugin_dir)
+    end)
   end)
 
-  it('warns the user if the plugin directory does not exist', function()
-    local plugin_dir = vim.fs.joinpath(base_dir, 'test-plugin')
-    local yazi_dir = vim.fs.joinpath(base_dir, 'fake-yazi-dir')
+  describe('installing a flavor', function()
+    it('can install if everything goes well', function()
+      local flavor_dir = vim.fs.joinpath(base_dir, 'test-flavor')
+      local yazi_dir = vim.fs.joinpath(base_dir, 'fake-yazi-dir')
 
-    -- NOTE: we are not creating the plugin directory
-    -- vim.fn.mkdir(plugin_dir)
-    vim.fn.mkdir(yazi_dir)
-    vim.fn.mkdir(vim.fs.joinpath(yazi_dir, 'plugins'))
+      vim.fn.mkdir(flavor_dir)
+      vim.fn.mkdir(yazi_dir)
 
-    local result = plugin.build_plugin({
-      dir = plugin_dir,
-      name = 'test-plugin-2',
-    }, { yazi_dir = yazi_dir })
+      plugin.build_flavor({
+        dir = flavor_dir,
+        name = 'test-flavor',
+      }, { yazi_dir = yazi_dir })
 
-    assert.is_equal(result.error, 'plugin directory does not exist')
+      local symlink =
+        vim.uv.fs_readlink(vim.fs.joinpath(yazi_dir, 'flavors', 'test-flavor'))
+
+      assert.are.same(flavor_dir, symlink)
+    end)
   end)
 end)
