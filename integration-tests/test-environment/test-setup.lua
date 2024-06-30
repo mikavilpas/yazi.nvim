250df4e4a1988505230c11a6bd36dad7c34ce47e
Mika Vilpas <mika.vilpas@gmail.com>
2024-06-30 15:39:58 +0300

    fixup! test: add integration tests using cypress and a custom terminal view

--- integration-tests/test-environment/test-setup.lua
+++ integration-tests/test-environment/test-setup.lua
@@ -28,6 +28,8 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+vim.opt.rtp:prepend('../../')
+
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
