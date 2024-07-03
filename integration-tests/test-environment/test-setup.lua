a2f7832af7462c79d57777dd8c36ad0edfb84b39
Mika Vilpas <mika.vilpas@gmail.com>
2024-07-03 17:11:55 +0300

    ci: make integration-tests more robust
    Fixes the following issues:
    - an extra `cypress open` was launched when running tests in ci. They
      should have been run headlessly.
    - swapfiles were occasionally created in the test environment, which
      could cause tests to fail. This only happened developing locally.

--- integration-tests/test-environment/test-setup.lua
+++ integration-tests/test-environment/test-setup.lua
@@ -35,6 +35,7 @@ vim.opt.rtp:prepend('../../')
 -- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+vim.o.swapfile = false
 
 -- install the following plugins
 ---@type LazySpec
