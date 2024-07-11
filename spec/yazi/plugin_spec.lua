0e795143ac53d8805e9b08cf2454c55cb3a6a83a
Mika Vilpas <mika.vilpas@gmail.com>
2024-07-11 07:57:11 +0300

    feat(plugin): allow specifying a subdirectory for plugins
    Some plugins are not found at the root of the repository, but in a
    subdirectory. This change allows specifying a subdirectory when
    installing a plugin.
    
    It corresponds to using `#` in the plugin URL when installing plugins
    without yazi.nvim with the `ya pack` command.
    
    ```sh
    ya pack -a redbeardymcgee/yazi-plugins#keyjump
    ```
    
    Is the same as:
    
    ```lua
    ---@type LazySpec
    {
      "redbeardymcgee/yazi-plugins",
      lazy = true,
      build = function(plugin)
        require("yazi.plugin").build_plugin(plugin, { sub_dir = "keyjump.yazi" })
      end,
    },
    ```

--- spec/yazi/plugin_spec.lua
+++ spec/yazi/plugin_spec.lua
@@ -53,6 +53,28 @@ describe('installing a plugin', function()
       assert.is_equal(result.error, 'source directory does not exist')
       assert.is_equal(result.from, plugin_dir)
     end)
+
+    it('can install a plugin from a monorepo subdirectory', function()
+      local plugin_monorepo_dir = vim.fs.joinpath(base_dir, 'yazi-plugins')
+      local plugin_dir = vim.fs.joinpath(plugin_monorepo_dir, 'keyjump.yazi')
+      local yazi_dir = vim.fs.joinpath(base_dir, 'fake-yazi-dir')
+
+      vim.fn.mkdir(plugin_monorepo_dir)
+      vim.fn.mkdir(plugin_dir)
+      vim.fn.mkdir(yazi_dir)
+      vim.fn.mkdir(vim.fs.joinpath(yazi_dir, 'plugins'))
+
+      plugin.build_plugin({
+        dir = plugin_monorepo_dir,
+        name = 'yazi-plugins',
+      }, { yazi_dir = yazi_dir, sub_dir = 'keyjump.yazi' })
+
+      -- verify that the plugin was symlinked
+      local symlink =
+        vim.uv.fs_readlink(vim.fs.joinpath(yazi_dir, 'plugins', 'keyjump.yazi'))
+
+      assert.are.same(plugin_dir, symlink)
+    end)
   end)
 
   describe('installing a flavor', function()
@@ -70,6 +92,29 @@ describe('installing a plugin', function()
 
       local symlink =
         vim.uv.fs_readlink(vim.fs.joinpath(yazi_dir, 'flavors', 'test-flavor'))
+
+      assert.are.same(flavor_dir, symlink)
+    end)
+
+    it('can install a flavor from a monorepo subdirectory', function()
+      local flavor_monorepo_dir = vim.fs.joinpath(base_dir, 'yazi-flavors')
+      local flavor_dir = vim.fs.joinpath(flavor_monorepo_dir, 'flavor123.yazi')
+      local yazi_dir = vim.fs.joinpath(base_dir, 'fake-yazi-dir')
+
+      vim.fn.mkdir(flavor_monorepo_dir)
+      vim.fn.mkdir(flavor_dir)
+      vim.fn.mkdir(yazi_dir)
+      vim.fn.mkdir(vim.fs.joinpath(yazi_dir, 'flavors'))
+
+      plugin.build_flavor({
+        dir = flavor_monorepo_dir,
+        name = 'yazi-flavors',
+      }, { yazi_dir = yazi_dir, sub_dir = 'flavor123.yazi' })
+
+      -- verify that the flavor was symlinked
+      local symlink = vim.uv.fs_readlink(
+        vim.fs.joinpath(yazi_dir, 'flavors', 'flavor123.yazi')
+      )
 
       assert.are.same(flavor_dir, symlink)
     end)
