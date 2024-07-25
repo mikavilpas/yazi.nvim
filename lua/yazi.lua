a5f607ea6af53816e3fa4db452117800e370c2bb
Mika Vilpas <mika.vilpas@gmail.com>
2024-07-25 22:09:25 +0300

    chore(main): release 3.1.5 (#264)

--- lua/yazi.lua
+++ lua/yazi.lua
@@ -4,7 +4,7 @@ local configModule = require('yazi.config')
 
 local M = {}
 
-M.version = '3.1.4' -- x-release-please-version
+M.version = '3.1.5' -- x-release-please-version
 
 -- The last known state of yazi when it was closed
 ---@type YaziPreviousState
