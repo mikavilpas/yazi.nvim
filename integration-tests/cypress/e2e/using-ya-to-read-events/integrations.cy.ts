099d923a3e6e9464a556f3ec5b71f8fbc63d6085
Mika Vilpas <mika.vilpas@gmail.com>
2024-07-25 08:35:18 +0300

    fix: grug-far integration not being able to search outside of the cwd
    There was a bug in the grug-far integration (the default search and
    replace integration in yazi.nvim) where it was not able to search
    outside of the current working directory. This was due to the fact that
    the file path to search in was passed in `Files Filter` instead of in
    `Flags`, which is the correct place to pass the file path.
    
    https://github.com/MagicDuck/grug-far.nvim/issues/146

--- integration-tests/cypress/e2e/using-ya-to-read-events/integrations.cy.ts
+++ integration-tests/cypress/e2e/using-ya-to-read-events/integrations.cy.ts
@@ -23,12 +23,16 @@ describe("integrations to other tools", () => {
 
       // the directory we were in should be prefilled in grug-far.nvim's view
       cy.contains("testdirs")
-      const p = path.join(
-        dir.rootPathRelativeToTestEnvironmentDir,
-        "routes",
-        "**",
-      )
+      const p = path.join(dir.rootPathRelativeToTestEnvironmentDir, "routes")
       cy.contains(p)
+
+      // by default, the focus is on the search field in normal mode. Type
+      // something in the search field so we can see if results can be found
+      cy.typeIntoTerminal("ithis")
+
+      // maybe we don't want to make too many assertions on code we don't own
+      // though, so for now we trust that it works in CI, and can verify it
+      // works locally
     })
   })
 })
