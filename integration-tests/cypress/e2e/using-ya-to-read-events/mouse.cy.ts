6a9d21bad8b0921ca668f0678bbfab41e8ea34ba
Mika Vilpas <mika.vilpas@gmail.com>
2024-07-26 21:49:38 +0300

    fix: close the floating terminal if it loses focus
    The floating terminal that shows yazi running can lose focus when e.g.
    clicked outside of. This commit adds an autocmd that closes the floating
    terminal in these cases.
    
    The reason for this is that it's inconvenient to navigate back to the
    floating terminal to close it manually. Even if you did this, it was in
    normal mode - not insert mode which is typically what you want to be in
    when you're using yazi.

---
+++ integration-tests/cypress/e2e/using-ya-to-read-events/mouse.cy.ts
@@ -0,0 +1,28 @@
+import { startNeovimWithYa } from "./startNeovimWithYa"
+
+describe("mouse support", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:5173")
+  })
+
+  it("can use grug-far.nvim to search and replace in the cwd", () => {
+    startNeovimWithYa().then((dir) => {
+      // wait until text on the start screen is visible
+      cy.contains("If you see this text, Neovim is ready!")
+
+      // open yazi
+      cy.typeIntoTerminal("{upArrow}")
+
+      // yazi should be showing adjacent files
+      cy.contains(dir.contents["test.lua"].name)
+
+      // click outside of the yazi floating window. This should close it
+      // because it's designed to close when it loses focus
+      cy.contains("-- TERMINAL --").click()
+
+      // clicking outside of the yazi window should close it, after which
+      // Neovim should not be showing the TERMINAL buffer any longer
+      cy.contains("-- TERMINAL --").should("not.exist")
+    })
+  })
+})
