562a04b343eb2ee3351a05800bbb922bf5ef479e
Mika Vilpas <mika.vilpas@gmail.com>
2024-07-27 08:31:39 +0300

    fixup! test: can open a directory with `:edit .`

--- integration-tests/cypress/e2e/using-shell-redirection-to-read-events/opening-directories.cy.ts
+++ integration-tests/cypress/e2e/using-shell-redirection-to-read-events/opening-directories.cy.ts
@@ -7,8 +7,21 @@ describe("opening directories", () => {
       filename: ".",
     }).then((dir) => {
       // yazi should now be visible, showing the names of adjacent files
-      cy.contains(dir.contents["file.txt"].name)
+      cy.contains(dir.contents["test-setup.lua"].name)
+    })
+  })
+
+  it("can open a directory with `:edit .`", () => {
+    cy.visit("http://localhost:5173")
+    cy.startNeovim().then((dir) => {
+      // yazi should now be visible, showing the names of adjacent files
       cy.contains(dir.contents["initial-file.txt"].name)
+
+      // open the current directory using a command
+      cy.typeIntoTerminal(":edit .{enter}")
+
+      // yazi should now be visible, showing the names of adjacent files
+      cy.contains(dir.contents["test-setup.lua"].name)
     })
   })
 })
