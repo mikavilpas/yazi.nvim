ce858cf8be5cd1e811c1872a5bafddce95a4a972
Mika Vilpas <mika.vilpas@gmail.com>
2024-07-27 08:27:22 +0300

    test: can open a directory with `:edit .`

--- integration-tests/cypress/e2e/using-ya-to-read-events/opening-directories.cy.ts
+++ integration-tests/cypress/e2e/using-ya-to-read-events/opening-directories.cy.ts
@@ -14,4 +14,18 @@ describe("opening directories", () => {
       cy.typeIntoTerminal("{downArrow}")
     })
   })
+
+  it("can open a directory with `:edit .`", () => {
+    cy.visit("http://localhost:5173")
+    startNeovimWithYa().then((dir) => {
+      // yazi should now be visible, showing the names of adjacent files
+      cy.contains(dir.contents["initial-file.txt"].name)
+
+      // open the current directory using a command
+      cy.typeIntoTerminal(":edit .{enter}")
+
+      // yazi should now be visible, showing the names of adjacent files
+      cy.contains(dir.contents["test-setup.lua"].name)
+    })
+  })
 })
