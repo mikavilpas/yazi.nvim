3fbb0a989473149144ede43d2e7be0deff687b2d
xfzv <78810647+xfzv@users.noreply.github.com>
2024-08-03 14:54:51 +0000

    refactor: use `vim.lsp.get_clients()` instead of deprecated `vim.lsp.get_active_clients()` (#307)
    `vim.lsp.get_active_clients()` is deprecated and will be removed in
    Neovim 0.12. This causes deprecation warning on 0.11 after renaming
    files with bulk renaming feature.
    
    ref:
    https://github.com/neovim/neovim/blob/512d228111bccf3e52613c798edc7f803c1de13f/runtime/doc/deprecated.txt#L52
    
    Co-authored-by: xfzv <>

--- lua/yazi/lsp/embedded/lsp-file-operations/will-delete.lua
+++ lua/yazi/lsp/embedded/lsp-file-operations/will-delete.lua
@@ -33,7 +33,8 @@ local function getWorkspaceEdit(client, fname)
 end
 
 M.callback = function(data)
-  for _, client in pairs(vim.lsp.get_active_clients()) do
+  local clients = vim.lsp.get_clients or vim.lsp.get_active_clients
+  for _, client in pairs(clients()) do
     local will_delete = utils.get_nested_path(
       client,
       { 'server_capabilities', 'workspace', 'fileOperations', 'willDelete' }
