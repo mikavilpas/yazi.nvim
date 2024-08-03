03c5bbe9e8d85df8bbe5e532b624becfe1c5369a
Mika Vilpas <mika.vilpas@gmail.com>
2024-08-03 21:39:04 +0300

    docs: mention extra dependencies in README (#313)
    Closes https://github.com/mikavilpas/yazi.nvim/issues/306

--- lazy.lua
+++ lazy.lua
@@ -1,6 +1,9 @@
 -- This file is used to define the dependencies of this plugin when the user is
 -- using lazy.nvim.
 --
+-- If you are curious about how exactly the plugins are used, you can use e.g.
+-- the search functionality on Github.
+--
 --https://lazy.folke.io/packages#lazy
 
 ---@module "lazy"
@@ -8,7 +11,16 @@
 
 ---@type LazySpec
 return {
+  -- Needed for file path resolution mainly
+  --
+  -- https://github.com/nvim-lua/plenary.nvim/
   { 'nvim-lua/plenary.nvim', lazy = true },
+
+  -- Needed to calculate a good buffer background color that fits the current
+  -- theme when yazi hovers over a file in an open window/split. The plugin
+  -- itself is never actually loaded, only some utilities that it comes with.
+  --
+  -- https://github.com/akinsho/bufferline.nvim
   { 'akinsho/bufferline.nvim', lazy = true },
 
   --
