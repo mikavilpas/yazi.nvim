d749bb412a03db63b6d98890a4557f0c6b1c921c
Mika Vilpas <mika.vilpas@gmail.com>
2024-08-07 20:12:27 +0300

    chore: update repro script to use recommended commands (#337)

--- repro.lua
+++ repro.lua
@@ -33,15 +33,22 @@ local plugins = {
     'mikavilpas/yazi.nvim',
     event = 'VeryLazy',
     keys = {
-      {
-        -- 👇 choose your own keymapping
-        '<leader>fy',
-        function()
-          require('yazi').yazi()
-        end,
-
-        { desc = 'Open the file manager' },
-      },
+      '<leader>-',
+      '<cmd>Yazi<cr>',
+      desc = 'Open yazi at the current file',
+    },
+    {
+      -- Open in the current working directory
+      '<leader>cw',
+      '<cmd>Yazi cwd<cr>',
+      desc = "Open the file manager in nvim's working directory",
+    },
+    {
+      -- NOTE: this requires a version of yazi that includes
+      -- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
+      '<c-up>',
+      '<cmd>Yazi toggle<cr>',
+      desc = 'Resume the last yazi session',
     },
     ---@type YaziConfig
     opts = {
