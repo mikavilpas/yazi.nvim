87c08c6e14490bd22eaddaae18cd3a49ed26df81
Mika Vilpas <mika.vilpas@gmail.com>
2024-08-11 13:44:50 +0300

    test: adapt to multiple hover messages being sent
    This seems to occur after https://github.com/sxyazi/yazi/pull/1468 but
    doesn't actually cause any issues. Maybe it had some side effect.

--- integration-tests/test-environment/config-modifications/notify_hover_events.lua
+++ integration-tests/test-environment/config-modifications/notify_hover_events.lua
@@ -7,8 +7,7 @@ vim.api.nvim_create_autocmd("User", {
   -- see `:help event-args`
   ---@param event {data: YaziHoverEvent}
   callback = function(event)
-    vim.notify(
-      vim.inspect({ "Just received a YaziDDSHover event!", event.data })
-    )
+    -- printing the messages will allow seeing them with `:messages`
+    print(vim.inspect({ "Just received a YaziDDSHover event!", event.data }))
   end,
 })
