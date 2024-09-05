4b8060a42c0bf390036975e291952cd4e23a8016
Mika Vilpas <mika.vilpas@gmail.com>
2024-09-05 21:33:00 +0300

    fix: `nvim .` sent extra `i` key to yazi
    Issue
    =
    
    When opening yazi with `nvim .`, an extra `i` key was sent yazi. If the
    user had set up a custom mapping for `i` in their yazi config, it would
    be triggered when opening yazi with `nvim .`. In other cases this bug
    would not get triggered.
    
    Solution
    =
    
    Check if we're in the situation where the `i` key needs to be pressed
    (to enter insert mode) and only then send the key.

--- lua/yazi/hijack_netrw.lua
+++ lua/yazi/hijack_netrw.lua
@@ -7,51 +7,55 @@ function M.hijack_netrw(yazi_augroup)
   ---@param file string
   ---@param bufnr number
   local function open_yazi_in_directory(file, bufnr)
-    if vim.fn.isdirectory(file) == 1 then
-      local winid = vim.api.nvim_get_current_win()
-      local dir_bufnr = vim.api.nvim_get_current_buf()
-
-      -- A buffer was opened for a directory.
-      -- Remove the buffer as we want to show yazi instead
-      local empty_buffer = vim.api.nvim_create_buf(true, false)
-      local next_buffer = vim.fn.bufnr("#") or empty_buffer
-      Log:debug(
-        string.format(
-          "Removing buffer %s for directory %s and replacing it with the next buffer %s",
-          dir_bufnr,
-          file,
-          next_buffer
-        )
-      )
-      vim.schedule(function()
-        Log:debug(
-          string.format("Deleting buffer %s for directory %s", dir_bufnr, file)
-        )
-
-        pcall(function()
-          vim.api.nvim_win_set_buf(winid, next_buffer)
-          Log:debug(
-            string.format("Set buffer %s for window %s", next_buffer, winid)
-          )
-        end)
-        vim.api.nvim_buf_delete(bufnr, { force = true })
-        if next_buffer ~= empty_buffer then
-          vim.api.nvim_buf_delete(empty_buffer, { force = true })
-        end
-        Log:debug(
-          string.format("Deleted buffer %s for directory %s", bufnr, file)
-        )
-
-        Log:debug(string.format("Opening yazi for directory %s", file))
-        require("yazi").yazi(M.config, file)
-
-        -- HACK: for some reason, the cursor is not in insert mode when opening
-        -- yazi from the command line with `neovim .`, so just simulate
-        -- pressing "i" to enter insert mode :) It did nothing when when I
-        -- tried using vim.cmd('startinsert') or vim.cmd('normal! i')
-        vim.api.nvim_feedkeys("i", "n", false)
-      end)
+    if vim.fn.isdirectory(file) ~= 1 then
+      return
     end
+
+    local winid = vim.api.nvim_get_current_win()
+    local dir_bufnr = vim.api.nvim_get_current_buf()
+
+    -- A buffer was opened for a directory.
+    -- Remove the buffer as we want to show yazi instead
+    local empty_buffer = vim.api.nvim_create_buf(true, false)
+    local next_buffer = vim.fn.bufnr("#") or empty_buffer
+    Log:debug(
+      string.format(
+        "Removing buffer %s for directory %s and replacing it with the next buffer %s",
+        dir_bufnr,
+        file,
+        next_buffer
+      )
+    )
+    vim.schedule(function()
+      Log:debug(
+        string.format("Deleting buffer %s for directory %s", dir_bufnr, file)
+      )
+
+      pcall(function()
+        vim.api.nvim_win_set_buf(winid, next_buffer)
+        Log:debug(
+          string.format("Set buffer %s for window %s", next_buffer, winid)
+        )
+      end)
+      vim.api.nvim_buf_delete(bufnr, { force = true })
+      if next_buffer ~= empty_buffer then
+        vim.api.nvim_buf_delete(empty_buffer, { force = true })
+      end
+      Log:debug(
+        string.format("Deleted buffer %s for directory %s", bufnr, file)
+      )
+
+      Log:debug(string.format("Opening yazi for directory %s", file))
+      require("yazi").yazi(M.config, file)
+
+      -- HACK: for some reason, the cursor is not in insert mode when opening
+      -- yazi from the command line with `neovim .`, so just simulate
+      -- pressing "i" to enter insert mode :) It did nothing when when I
+      -- tried using vim.cmd('startinsert') or vim.cmd('normal! i')
+      if vim.fn.mode(true) == "t" then
+        vim.api.nvim_feedkeys("i", "n", false)
+      end
+    end)
   end
 
   -- disable netrw, the built-in file explorer
