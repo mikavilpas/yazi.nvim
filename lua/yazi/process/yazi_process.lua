445f4877d8b80f9d24ea1f9b890c878211878d63
Mika Vilpas <mika.vilpas@gmail.com>
2024-09-21 09:55:57 +0300

    feat: expose the current working directory to keybindings (#471)
    This is a technical change that allows keybindings to access the current
    working directory of the ya process. It can be used to solve
    https://github.com/mikavilpas/yazi.nvim/issues/466 in the future

--- lua/yazi/process/yazi_process.lua
+++ lua/yazi/process/yazi_process.lua
@@ -8,7 +8,7 @@ local YaziProcessApi = require("yazi.process.yazi_process_api")
 ---@class YaziProcess
 ---@field public api YaziProcessApi
 ---@field public yazi_job_id integer
----@field private event_reader YaProcess "The process that reads events from yazi"
+---@field public ya_process YaProcess "The process that reads events from yazi"
 local YaziProcess = {}
 
 ---@diagnostic disable-next-line: inject-field
@@ -26,25 +26,25 @@ function YaziProcess:start(config, paths, on_exit)
   local yazi_id = string.format("%.0f", vim.uv.hrtime())
   self.api = YaziProcessApi.new(config, yazi_id)
 
-  self.event_reader = YaProcess.new(config, yazi_id)
+  self.ya_process = YaProcess.new(config, yazi_id)
 
-  local yazi_cmd = self.event_reader:get_yazi_command(paths)
+  local yazi_cmd = self.ya_process:get_yazi_command(paths)
   Log:debug(string.format("Opening yazi with the command: (%s).", yazi_cmd))
 
   self.yazi_job_id = vim.fn.termopen(yazi_cmd, {
     on_exit = function(_, code)
-      self.event_reader:kill()
-      local events = self.event_reader:wait(1000)
+      self.ya_process:kill()
+      local events = self.ya_process:wait(1000)
 
       local chosen_files = {}
       if utils.file_exists(config.chosen_file_path) == true then
         chosen_files = vim.fn.readfile(config.chosen_file_path)
       end
-      on_exit(code, chosen_files, events, self.event_reader.hovered_url)
+      on_exit(code, chosen_files, events, self.ya_process.hovered_url)
     end,
   })
 
-  self.event_reader:start()
+  self.ya_process:start()
 
   return self
 end
--- spec/yazi/ya_process_spec.lua
+++ spec/yazi/ya_process_spec.lua
@@ -70,3 +70,47 @@ describe("the get_yazi_command() function", function()
     )
   end)
 end)
+
+describe("process_events()", function()
+  describe("cd events", function()
+    -- processing cd events is important so that keymaps can retrieve the cwd
+    -- and operate on it
+
+    local config = require("yazi.config").default()
+
+    it("stores the current working directory (cwd)", function()
+      local ya = ya_process.new(config, "yazi_id_123")
+
+      ya:process_events({
+        {
+          type = "cd",
+          timestamp = "2021-09-01T12:00:00Z",
+          id = "cd_123",
+          url = "/tmp",
+        } --[[@as YaziChangeDirectoryEvent]],
+      })
+
+      assert.are.same("/tmp", ya.cwd)
+    end)
+
+    it("overrides the previous cwd when it's changed multiple times", function()
+      local ya = ya_process.new(config, "yazi_id_123")
+      ya:process_events({
+        {
+          type = "cd",
+          timestamp = "2021-09-01T12:00:00Z",
+          id = "cd_123",
+          url = "/tmp",
+        } --[[@as YaziChangeDirectoryEvent]],
+        {
+          type = "cd",
+          timestamp = "2021-09-01T12:00:00Z",
+          id = "cd_123",
+          url = "/tmp/directory",
+        } --[[@as YaziChangeDirectoryEvent]],
+      })
+
+      assert.are.same("/tmp/directory", ya.cwd)
+    end)
+  end)
+end)
