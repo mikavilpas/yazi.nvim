50fc963539dba15f66793f45986836d981169114
Mika Vilpas <mika.vilpas@gmail.com>
2025-01-11 20:55:54 +0200

    test: add e2e test for renaming files in yazi with LSP support

--- integration-tests/test-environment/.config/nvim/prepare.lua
+++ integration-tests/test-environment/.config/nvim/prepare.lua
@@ -7,3 +7,21 @@
 
 -- make sure the dependencies defined in ./init.lua are installed
 vim.cmd("Lazy! sync")
+
+-- Install an LSP server for use in end-to-end tests. Here, the server will be
+-- put under the .repro/ directory, so it only needs to be installed once when
+-- the entire test run is being started. The tests can reuse the LSP server
+-- without re-downloading it.
+--
+-- The recommendation is to add a specific version to avoid issues in the
+-- future
+require("mason")
+require("mason-lspconfig").setup({
+  -- TODO why does automatic_installation not work?
+  -- ensure_installed = { "lua_ls@3.13.5" },
+  -- automatic_installation = true,
+})
+
+-- TODO this seems to report some minor error but it works after that. Should
+-- clean this up, though.
+vim.cmd("LspInstall lua_ls@3.13.5")
---
+++ integration-tests/test-environment/lua-project/config.lua
@@ -0,0 +1,8 @@
+local config = {}
+
+-- the default configuration
+config.defaults = {}
+
+local ready
+
+return config
