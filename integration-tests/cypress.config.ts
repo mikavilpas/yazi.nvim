f35c6f5eb9895626b5a2724ec350a95182f08b5b
Mika Vilpas <mika.vilpas@gmail.com>
2025-03-08 09:10:59 +0200

    ci: save video for failed cypress tests

--- integration-tests/cypress.config.ts
+++ integration-tests/cypress.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "cypress"
+import fs from "fs"
 import { readFile, rm } from "fs/promises"
 import path from "path"
 import { fileURLToPath } from "url"
@@ -17,7 +18,22 @@ console.log(`yaziLogFile: ${yaziLogFile}`)
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
+    video: true,
     setupNodeEvents(on, _config) {
+      on("after:spec", (_spec, results): void => {
+        // https://docs.cypress.io/app/guides/screenshots-and-videos#Delete-videos-for-specs-without-failing-or-retried-tests
+        if (results && results.video) {
+          // Do we have failures for any retry attempts?
+          const failures = results.tests.some((test) => {
+            return test.attempts.some((attempt) => attempt.state === "failed")
+          })
+          if (!failures && fs.existsSync(results.video)) {
+            // delete the video if the spec passed and no tests retried
+            return fs.unlinkSync(results.video)
+          }
+        }
+      })
+
       on("task", {
         async removeYaziLog(): Promise<null> {
           try {
