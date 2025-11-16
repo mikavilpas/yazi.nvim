f9b9b302af0e2972db3cbe96caa9948fd9b8a1bf
Fusora <ask4fusora+github@gmail.com>
2025-11-16 11:32:46 +0700

    Change `yazi.openers` module to use `vim.fs.normalize`
    This should ensure the functionality of `yazi.openers` module.

--- lua/yazi/openers.lua
+++ lua/yazi/openers.lua
@@ -2,37 +2,37 @@ local M = {}
 
 ---@param chosen_file string
 function M.open_file(chosen_file)
-  vim.cmd(string.format("edit %s", vim.fs.normalize(vim.fn.fnameescape(chosen_file))))
+  vim.cmd(string.format("edit %s", vim.fs.normalize(chosen_file)))
 end
 
 ---@param chosen_file string
 function M.open_file_in_vertical_split(chosen_file)
-  local is_directory = vim.fn.isdirectory(chosen_file) == 1
+  local is_directory = vim.fs.isdirectory(chosen_file) == 1
   if not is_directory then
-    vim.cmd(string.format("vsplit %s", vim.fn.fnameescape(chosen_file)))
+    vim.cmd(string.format("vsplit %s", vim.fs.normalize(chosen_file)))
   end
 end
 
 ---@param chosen_file string
 function M.open_file_in_horizontal_split(chosen_file)
-  local is_directory = vim.fn.isdirectory(chosen_file) == 1
+  local is_directory = vim.fs.isdirectory(chosen_file) == 1
   if not is_directory then
-    vim.cmd(string.format("split %s", vim.fn.fnameescape(chosen_file)))
+    vim.cmd(string.format("split %s", vim.fs.normalize(chosen_file)))
   end
 end
 
 ---@param chosen_file string
 function M.open_file_in_tab(chosen_file)
-  local is_directory = vim.fn.isdirectory(chosen_file) == 1
+  local is_directory = vim.fs.isdirectory(chosen_file) == 1
   if not is_directory then
-    vim.cmd(string.format("tabedit %s", vim.fn.fnameescape(chosen_file)))
+    vim.cmd(string.format("tabedit %s", vim.fs.normalize(chosen_file)))
   end
 end
 
 ---@param chosen_files string[]
 function M.open_multiple_files(chosen_files)
-  local quoted = vim.tbl_map(vim.fn.fnameescape, chosen_files)
-  vim.cmd("args" .. table.concat(quoted, " "))
+  local quoted = vim.tbl_map(vim.fs.normalize, chosen_files)
+  vim.cmd.args({ table.concat(quoted, " ") })
 end
 
 ---@param chosen_files string[]
@@ -40,16 +40,16 @@ function M.send_files_to_quickfix_list(chosen_files)
   vim.fn.setqflist({}, "r", {
     title = "Yazi",
     items = vim.tbl_map(function(file)
-      -- append / to directories
-      local path = file
-      if vim.fn.isdirectory(file) == 1 then
-        path = file .. "/"
+      local path = vim.fs.normalize(file)
+
+      if vim.fn.isdirectory(path) == 1 then
+        path = path .. "/"
       end
 
       ---@type vim.quickfix.entry
       return {
-        filename = path,
-        text = path,
+        filename = path, -- This is true path
+        text = path,     -- This is for full path display only
         col = 1,
         lnum = 1,
       }
