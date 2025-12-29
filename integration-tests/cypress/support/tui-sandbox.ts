37dd3a2f32f9f9a2b135743b6216c9fd93777a15
Mika Vilpas <mika.vilpas@gmail.com>
2025-12-29 11:45:45 +0200

    chore: update @tui-sandbox/library to 12.2.2

--- integration-tests/cypress/support/tui-sandbox.ts
+++ integration-tests/cypress/support/tui-sandbox.ts
@@ -42,6 +42,12 @@ export type TerminalTestApplicationContext = {
 
   /** The test directory, providing type-safe access to its file and directory structure */
   dir: TestDirectory<MyTestDirectory>
+
+  /** Access to the clipboard of the terminal */
+  clipboard: {
+    system(): Cypress.Chainable<string>
+    primary(): Cypress.Chainable<string>
+  }
 }
 
 /** The api that can be used in tests after a Neovim instance has been started. */
@@ -88,6 +94,12 @@ export type NeovimContext = {
 
   /** The test directory, providing type-safe access to its file and directory structure */
   dir: TestDirectory<MyTestDirectory>
+
+  /** Access to the clipboard of the terminal */
+  clipboard: {
+    system(): Cypress.Chainable<string>
+    primary(): Cypress.Chainable<string>
+  }
 }
 
 /** Arguments for starting the neovim server. They are built based on your test
@@ -148,6 +160,15 @@ Cypress.Commands.add(
           cy.typeIntoTerminal(text, options)
         },
         dir: underlyingNeovim.dir as TestDirectory<MyTestDirectory>,
+
+        clipboard: {
+          primary() {
+            return cy.then(() => underlyingNeovim.clipboard.primary())
+          },
+          system() {
+            return cy.then(() => underlyingNeovim.clipboard.system())
+          },
+        },
       }
 
       return api
@@ -187,6 +208,14 @@ Cypress.Commands.add(
         },
         typeIntoTerminal(text, options) {
           cy.typeIntoTerminal(text, options)
+        },
+        clipboard: {
+          primary() {
+            return cy.then(() => terminal.clipboard.primary())
+          },
+          system() {
+            return cy.then(() => terminal.clipboard.system())
+          },
         },
       }
 
