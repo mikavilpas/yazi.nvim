a37e7bddd15abdce3bc49c42108dcc7b9b470825
Mika Vilpas <mika.vilpas@gmail.com>
2026-07-03 13:28:57 +0300

    test: only load nvim.yazi if running embedded within yazi.nvim (#2013)

--- integration-tests/test-environment/.config/yazi/init.lua
+++ integration-tests/test-environment/.config/yazi/init.lua
@@ -1,4 +1,7 @@
-local ok, nvim_plugin = pcall(require, "nvim")
-if ok then
-  nvim_plugin:setup()
+if os.getenv("YAZI_NVIM_ID") ~= nil then
+  -- load nvim.yazi only if running embedded within yazi.nvim
+  local ok, nvim_plugin = pcall(require, "nvim")
+  if ok then
+    nvim_plugin:setup()
+  end
 end
